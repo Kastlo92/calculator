d0a9bebb5eabdd367704649650e4a29b857e12b3
Kastlo92 <castellanimichele92@gmail.com>
2025-01-21 20:06:38 +0100

    updated screen using state. Created a function to handle the button presses (changing the displayed number and store the operator).  Need to revise some names.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import { useState } from "react";
+
 export default function App() {
   let result;
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const [screen, setScreen] = useState(0);
+  const [firstOperator, setFirstOperator] = useState(0);
+  const [secondOperator, setSecondOperator] = useState(0);
+
   return (
     <div>
-      <Screen result={result} />
+      <Display />
       {numbers.map((number) => (
         <Button num={number} key={number} />
       ))}
     </div>
   );
-}
 
-function Button(props) {
-  return (
-    <>
-      <button>{props.num}</button>
-    </>
-  );
-}
+  function Button(props) {
+    return (
+      <>
+        <button onClick={() => bPress}>{props.num}</button>
+      </>
+    );
+  }
 
-function Screen(props) {
-  return <h1>{props.result}</h1>;
+  function Display() {
+    return <h1>{screen}</h1>;
+  }
+
+  function bPress(props) {
+    setScreen(1);
+  }
 }
